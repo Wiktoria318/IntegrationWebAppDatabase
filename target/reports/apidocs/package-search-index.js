adaf738d9185963edfe8241480003fb813be53b1
Wiktoria Mostowska <73172954+Wiktoria318@users.noreply.github.com>
2025-02-02 18:47:33 +0100

    Add files via upload

---
+++ target/reports/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"pl.polsl.mostowska.integrationwebapp"},{"l":"pl.polsl.mostowska.integrationwebapp.model"},{"l":"pl.polsl.mostowska.integrationwebapp.resources"},{"l":"pl.polsl.mostowska.integrationwebapp.servlet"}];updateSearchResults();
